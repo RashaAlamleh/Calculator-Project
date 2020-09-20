b792b10243e66bc8742cf821f3471fa13dbb31af
ameedasmah <ameed.asmah@hotmail.com>
2020-09-20 18:11:41 +0300

    the change in css inside js file

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// function add(num1,num2){
-// var result = 0;
-// return result=num1+num2;
-// };
-
-// function sub(num1,num2){
-// var result = 0;
-// return result=num1-num2;
-// };
-
-// function multiple(num1,num2){
-// var result = 1;
-// return result=num1*num2;
-// };
-// function division(num1,num2){
-// var result = 1;
-// return result=num1/num2;
-// };
-
 
 function insrt(num){
   $('#inputVal').val($('#inputVal').val()+num)
@@ -26,7 +7,23 @@ function eql(){
 }
 
 function reset(){
-  document.getElementById(inputVal).vlaue('')
- // $('#inputVal').val('')
+  document.getElementById('inputVal').value = ''
 }
+// function pwo(num1,num2){
+//   document.getElementById(expo).value = Math.pwo(num1,num2)
+// }
 
+$('#cal').css({'font-style': 'italic','color':'#6a5acd'})
+
+$('body').css({'text-align':'center','background':'white'});
+
+$('.numOper').css({'margin-left': 'auto','margin-right': 'auto'
+});
+
+$('.btn').css({'width':'45px','height':'45px','font-size':'35', 'color':'white','margin':'auto','backgroundColor':'rgb(60, 60, 60)'});
+
+$('#inputVal').css('padding','10px');
+
+$('.all').css({'border': '5px solid rgb(60, 60, 60)',
+  'border-radius': '20px','box-sizing': 'border-box',
+  'width': '300px','height': '400px'});
