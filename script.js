d140510de2faebad9175cd39c95d1f45c3bc2ffd
Rasha <r.alamleh@gmail.com>
2020-09-21 11:42:48 +0300

    Final Update

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function each(coll, fn) {
 }
 
 function insrt(num){
-	debugger
   $('#inputVal').val($('#inputVal').val()+num)  
 }
+
 function eql(){
 	var x = $('#inputVal').val()
 	var arr = x.split('')
@@ -25,11 +25,19 @@ function eql(){
   $('#inputVal').val(eval(x))
 }
 
+var input = document.getElementById("inputVal");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("Equal").click();
+  }
+});
+
 function reset(){
   document.getElementById('inputVal').value = ''
 }
 
-$('#cal').css({'font-style': 'italic','color':'#6a5acd'})
+$('#cal').css({'font-style': 'italic','color':'rgb(231, 80, 33)'})
 $('body').css({
 'text-align':'center',
 'background':'white',
@@ -44,15 +52,19 @@ $('.numOper').css({'margin-left': 'auto','margin-right': 'auto'
 
 $('.btn').css({'width':'50px','height':'50px','font-size':'35px', 'color':'white','margin':'auto','backgroundColor':'rgb(60, 60, 60)'});
 
-$('#inputVal').css('padding','20px');
+$('#inputVal').css({'width':'200px','height':'40px'});
 
 $('.all').css({
   'border': '5px solid rgb(60, 60, 60)',
-  'width': '25%',
-  'height': '450px',
+  'width': '20%',
+  'height': '430px',
   'padding': '10px',
-  'margin': 'auto'
+  'margin': 'auto',
+  'margin-top':'70px',
+  'border-radius': '20px',
+  'backgroundColor':'rgb(10, 10, 10)'
 })
+$('#clear').css('backgroundColor','rgb(231, 80, 33)')
 
 // $('.all').css({'border': '5px solid rgb(60, 60, 60)',
 //   'border-radius': '20px','box-sizing': 'border-box',
